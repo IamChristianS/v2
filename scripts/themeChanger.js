66e4f46e2d9a150479d5fcf50e9e88ed99c5fb97
Christian Santangelo <95893289+unbl0ck@users.noreply.github.com>
2023-09-21 12:55:53 -0500

    ive been proven to be dense

--- scripts/themeChanger.js
+++ scripts/themeChanger.js
@@ -1,15 +1,8 @@
-// target the button using the data attribute we added earlier
 const button = document.querySelector("[data-theme-toggle]");
 
 button.addEventListener("click", () => {
   const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-
-  // Update Alt Text
-  const newCta = newTheme === "dark" ? "Enable Light Theme" : "Enable Dark Theme";
-  button.innerText = newCta;
-
-  button.setAttribute("aria-label", newCta);
-
+  
   // Update Theme Attribute on HTML
   document.querySelector("html").setAttribute("data-theme", newTheme);
 
