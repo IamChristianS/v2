a0cd136e5100a6baebdefb01e09385d8fc1b29f2
Christian Santangelo <95893289+unbl0ck@users.noreply.github.com>
2023-09-21 13:00:33 -0500

    flying spaghetti monter give me strengh

--- scripts/themeChanger.js
+++ scripts/themeChanger.js
@@ -1,14 +1,30 @@
+localStorage.getItem("theme");
+localStorage.setItem("theme", newTheme);
+
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+    if (localStorageTheme !== null) {
+        return localStorageTheme;
+    }
+    
+    if (systemSettingDark.matches) {
+        return "dark";
+    }
+
+    return "light";
+}
+
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
 const button = document.querySelector("[data-theme-toggle]");
 
 button.addEventListener("click", () => {
-  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-  
-  // Update Theme Attribute on HTML
-  document.querySelector("html").setAttribute("data-theme", newTheme);
+    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
 
-  // Update Local Storage (Cookies)
-  localStorage.setItem("theme", newTheme);
+    document.querySelector("html").setAttribute("data-theme", newTheme);
 
-  // Update Theme Settings in Memory
-  currentThemeSetting = newTheme;
+    localStorage.setItem("theme", newTheme);
+    currentThemeSetting = newTheme;
 });
