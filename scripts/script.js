39114cc4c0f341865d7e95ee0b9cfd1256d8bafe
Christian Santangelo <95893289+unbl0ck@users.noreply.github.com>
2023-09-22 10:04:38 -0500

    shortened code and adjusted stuff

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,8 @@
-// Settings Menu (poorly made by me)
+// Settings Menu
 var settingsDiv = document.querySelector('settings');
 
 function toggleSettings() {
-    if (settingsDiv.classList.contains('settings-animate')) {
-        settingsDiv.classList.remove("settings-animate");
-    } else {
-        settingsDiv.classList.add("settings-animate");
-    }
+    settingsDiv.classList.toggle("settings-animate");
 }
 
 // Theme Changer
