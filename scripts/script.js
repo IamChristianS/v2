94ef0f16d295e70807a03a49c67fab0c66bed736
Christian Santangelo <95893289+unbl0ck@users.noreply.github.com>
2023-09-22 13:23:23 -0500

    just testing new thingy

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
 // Settings Menu
+//function toggleSettings() {
+//    document.querySelector('settings').classList.toggle("settings-animate");
+//}
+
 function toggleSettings() {
-    document.querySelector('settings').classList.toggle("settings-animate");
+    this.__toggle = !this.__toggle;
+    var target = document.getElementById('settings');
+    if( this.__toggle) {
+        target.style.width = target.scrollWidth+"px";
+    }
+    else {
+        target.style.width = 0;
+    }
 }
 
 // Theme Changer
