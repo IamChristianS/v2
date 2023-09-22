1fa37ec9c46f64b43d1e2e7e709980d8d2a7df58
Christian Santangelo <95893289+unbl0ck@users.noreply.github.com>
2023-09-22 14:28:55 -0500

    maybe

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,15 @@
 // Settings Menu
 function toggleSettings() {
     var settingsMenu = document.getElementById("settings")
-    
+
     if (settingsMenu.style.width == "40vw") {
         settingsMenu.style.width = "0";
         settingsMenu.style.padding = "0";
+        settingsMenu.style.right = "0";
     } else {
         settingsMenu.style.width = "40vw";
         settingsMenu.style.padding = "2vw";
+        settingsMenu.style.right = "0";
     }
 }
 
