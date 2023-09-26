5508a66bd0bfd824608ca49b3380eb93303cd25e
Christian Santangelo <95893289+unbl0ck@users.noreply.github.com>
2023-09-26 14:39:57 -0500

    testing more

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 // Settings Menu
-var settingsMenu = document.getElementById("settings-modal");
-
 function openSettings() {
-  document.getElementById("settings-modal").style.display = "block";
+  document.getElementById("settings-modal")
+    .style.display = "block";
 }
 function closeSettings() {
-  document.getElementById("settings-modal").style.display = "none";
+  document.getElementById("settings-modal")
+    .style.display = "none";
 }
 
 // Theme Changer
