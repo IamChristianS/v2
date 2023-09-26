c83dccd3fcdd430ca1e507dc60772335cf83f92a
Christian Santangelo <95893289+unbl0ck@users.noreply.github.com>
2023-09-26 09:57:32 -0500

    testing new modal

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,17 @@
 // Settings Menu
-var settingsMenu = document.getElementById("settings")
-/*
-function toggleSettings() {
-    if (settingsMenu.style.width == "40vw") {
-        settingsMenu.style.width = "0";
-        settingsMenu.style.padding = "0";
-        settingsMenu.style.right = "0";
-    } else {
-        settingsMenu.style.width = "40vw";
-        settingsMenu.style.padding = "2vw";
-        settingsMenu.style.right = "0";
-    }
-}
-*/
+var settingsMenu = document.getElementById("settings");
 
-function openNav() {
-    settingsMenu.style.width = "250px";
+function openSettings() {
+  settingsMenu.style.display = "block";
+}
+function closeSettings() {
+  settingsMenu.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == settingsMenu) {
+    settingsMenu.style.display = "none";
+  }
 }
 
 // Theme Changer
