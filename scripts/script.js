22f0cde567a58d6ce4adce71b3cd36498b7bd95f
unbl0ck <95893289+unbl0ck@users.noreply.github.com>
2023-09-27 01:28:42 -0500

    thank god

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,24 @@
 // Settings Menu
-function openSettings() {
-  document.getElementById("settings-modal")
-    .style.display = "block";
-}
-function closeSettings() {
-  document.getElementById("settings-modal")
-    .style.display = "none";
+let settingsOpen = false
+
+function toggleSettings() {
+  const settingsModal = document.getElementById("settings-modal");
+  if (settingsOpen) {
+    settingsModal.style.display = "none";
+    settingsOpen = false;
+  } else {
+    settingsModal.style.display = "block";
+    settingsOpen = true;
+  }
 }
 
+document.addEventListener('click', function(event) {
+  const settingsModal = document.getElementById("settings-modal");
+  const settingsMenu = document.getElementById("settings");
+  if (settingsModal.contains(event.target) && !settingsMenu.contains(event.target)) {
+    toggleSettings()
+  }
+});
 // Theme Changer
 
 
