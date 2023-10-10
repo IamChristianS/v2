18fa59c8066a6826c03954e294d8e4093d9f17aa
unbl0ck <95893289+unbl0ck@users.noreply.github.com>
2023-10-09 22:41:35 -0500

    this hurts my brain but it works

--- scripts/panic.js
+++
@@ -1,2 +0,0 @@
-// Eventually will be a seperate script that detects if user presses panic button
-// Is used in ALL pages
--- scripts/script.js
+++ scripts/script.js
@@ -25,21 +25,74 @@ document.addEventListener('click', function(event) {
 });
 
 // Tab Masking
+function updateTitle() {
+  const title = document.getElementById('tabNameInput').value;
+  document.title = title;
+  localStorage.setItem('tabTitle', title);
+}
 
+function updateFavicon() {
+  const faviconUrl = document.getElementById('faviconInput').value;
+  const favicon = document.querySelector("link[rel='icon']") || document.createElement('link');
+  favicon.rel = 'icon';
+  favicon.type = 'image/png';
+  favicon.href = faviconUrl;
+  document.head.appendChild(favicon);
+  localStorage.setItem('faviconUrl', faviconUrl);
+}
+
+window.onload = function () {
+  const savedTitle = localStorage.getItem('tabTitle');
+  const savedFaviconUrl = localStorage.getItem('faviconUrl');
+
+  if (savedTitle) {
+      document.title = savedTitle;
+      document.getElementById('tabNameInput').value = savedTitle;
+  }
+
+  if (savedFaviconUrl) {
+      const favicon = document.querySelector("link[rel='icon']") || document.createElement('link');
+      favicon.rel = 'icon';
+      favicon.type = 'image/png';
+      favicon.href = savedFaviconUrl;
+      document.head.appendChild(favicon);
+      document.getElementById('faviconInput').value = savedFaviconUrl;
+  }
+};
 
 // About:Blank Cloak
 
 
-// Panic Button (ty chatgpt <3) ((Gonna need it again for setting it to localstorage :/))
+// Panic Button (ty chatgpt <3)
 document.addEventListener("DOMContentLoaded", function () {
-  let panicKey = null;
   const setPanicKeyButton = document.querySelector(".panic-button");
+  const resetPanicKeyButton = document.querySelector(".panic-reset-button");
   const displayPanicKey = document.querySelector(".panic-key-display");
-  const storedSavedKey = localStorage.getItem("savedKey");
+  let storedSavedKey = localStorage.getItem("savedKey");
+  let panicKey = null;
   const redirectURL = "https://www.google.com";
 
-  if (storedSavedKey != null) {
-    displayPanicKey.textContent = `Current Panic Key: ${storedSavedKey}`;
+  const keyNameMapping = { // I hate this (ty chatgpt for speeding this long a** process up)
+    KeyA: "A", KeyB: "B", KeyC: "C", KeyD: "D", KeyE: "E", KeyF: "F", KeyG: "G", KeyH: "H", KeyI: "I", KeyJ: "J", KeyK: "K", KeyL: "L", KeyM: "M", KeyN: "N", KeyO: "O", KeyP: "P", KeyQ: "Q", KeyR: "R", KeyS: "S", KeyT: "T", KeyU: "U", KeyV: "V", KeyW: "W", KeyX: "X", KeyY: "Y", KeyZ: "Z", 
+
+    Digit0: "0", Digit1: "1", Digit2: "2", Digit3: "3", Digit4: "4", Digit5: "5", Digit6: "6", Digit7: "7", Digit8: "8", Digit9: "9", 
+
+    F1: "F1", F2: "F2", F3: "F3", F4: "F4", F5: "F5", F6: "F6", F7: "F7", F8: "F8", F9: "F9", F10: "F10", F11: "F11", F12: "F12", F13: "F13", F14: "F14", F15: "F15", F16: "F16", F17: "F17", F18: "F18", F19: "F19", F20: "F20", F21: "F21", F22: "F22", F23: "F23", F24: "F24", 
+
+    Space: "Space", Enter: "Enter", Tab: "Tab", Backspace: "Backspace", Escape: "Escape", Delete: "Delete", CapsLock: "Caps Lock", ShiftLeft: "Left Shift", ShiftRight: "Right Shift", ControlLeft: "Left Ctrl", ControlRight: "Right Ctrl", AltLeft: "Left Alt", AltRight: "Right Alt", ArrowUp: "Up Arrow", ArrowDown: "Down Arrow", ArrowLeft: "Left Arrow", ArrowRight: "Right Arrow", 
+
+    Numpad0: "Numpad 0", Numpad1: "Numpad 1", Numpad2: "Numpad 2", Numpad3: "Numpad 3", Numpad4: "Numpad 4", Numpad5: "Numpad 5", Numpad6: "Numpad 6", Numpad7: "Numpad 7", Numpad8: "Numpad 8", Numpad9: "Numpad 9", NumpadAdd: "Numpad +", NumpadSubtract: "Numpad -", NumpadMultiply: "Numpad *", NumpadDivide: "Numpad /", NumpadEnter: "Numpad Enter", NumpadDecimal: "Numpad .", 
+
+    ArrowUp: "Up Arrow", ArrowDown: "Down Arrow", ArrowLeft: "Left Arrow", ArrowRight: "Right Arrow", 
+
+    ShiftLeft: "Left Shift", ShiftRight: "Right Shift", ControlLeft: "Left Ctrl", ControlRight: "Right Ctrl", AltLeft: "Left Alt", AltRight: "Right Alt", 
+    // If you use these,  I don't trust you
+    Home: "Home", End: "End", PageUp: "Page Up", PageDown: "Page Down", Insert: "Insert", Delete: "Delete", PrintScreen: "Print Screen", ScrollLock: "Scroll Lock", Pause: "Pause/Break", 
+    // Same goes for these
+    MediaPlay: "Play", MediaPause: "Pause", MediaStop: "Stop", MediaNextTrack: "Next Track", MediaPrevTrack: "Previous Track", MediaFastForward: "Fast Forward", MediaRewind: "Rewind", MediaEject: "Eject", 
+  };
+  function getKeyDisplayName(code) {
+    return keyNameMapping[code] || code;
   }
 
   function setPanicKey() {
@@ -48,26 +101,52 @@ document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener("keyup", recordPanicKey);
   }
   function recordPanicKey(event) {
-    const key = event.key.toUpperCase();
+    let key = event.key.toUpperCase();
     panicKey = event.code;
-
     localStorage.setItem("savedKey", panicKey);
+    storedSavedKey = panicKey;
+    key = getKeyDisplayName(panicKey);
 
-    displayPanicKey.textContent = `Current Panic Key: ${storedSavedKey}`;
+    displayPanicKey.textContent = `Current Panic Key: ${key}`;
     setPanicKeyButton.textContent = "Change Panic Key";
+
     document.removeEventListener("keyup", recordPanicKey);
+    updateEventListener();
   }
 
-  setPanicKeyButton.addEventListener("click", setPanicKey);
-
-  document.addEventListener("keydown", (event) => {
-    if (storedSavedKey && event.code === storedSavedKey) {  
-        window.location.href = redirectURL;
+  document.addEventListener("click", function () {
+    if (!settingsOpen) {
+      document.removeEventListener("keyup", recordPanicKey);
+    setPanicKeyButton.textContent = "Press to Record Key...";
     }
-});
+  });
+  resetPanicKeyButton.addEventListener("click", function () {
+    document.removeEventListener("keyup", recordPanicKey);
+    localStorage.removeItem("savedKey");
+    storedSavedKey = null;
+    setPanicKeyButton.textContent = "Press to Record Key...";
+    displayPanicKey.textContent = `Current Panic Key: Not Set`;
+  });
+
+  function updateEventListener() {
+    document.removeEventListener("keydown", checkPanicKey);
+    document.addEventListener("keydown", checkPanicKey);
+  }
+  function checkPanicKey(event) {
+    if (storedSavedKey && event.code === storedSavedKey) {  
+      window.location.href = redirectURL;
+    }
+  }
+  updateEventListener();
+  if (storedSavedKey != null) {
+    const displayName = getKeyDisplayName(storedSavedKey);
+    displayPanicKey.textContent = `Current Panic Key: ${displayName}`;
+    setPanicKeyButton.textContent = "Change Panic Key";
+  }
+  setPanicKeyButton.addEventListener("click", setPanicKey);
 });
 
-// Credits (ty stackoveflow user <3)
+// Credits (ty stackoverflow user <3)
 let creditsDropped = false
 
 function toggleCredits() {
