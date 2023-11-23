6d780ac28ac09a1c8469b6a880c6d565d9418082
unbl0ck <95893289+unbl0ck@users.noreply.github.com>
2023-11-22 22:57:14 -0600

    more testing

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -196,3 +196,22 @@ function toggleTranslate() {
   }
 }
 //////////////////////////////////////////////////
+// Mobile
+let navOpen = false
+
+function toggleMobileNav() {
+    const navOut = document.getElementById("mobile-nav-out");
+    const navBtn = document.getElementById("mobile-open-nav");
+    if (navOpen) {
+        navOut.style.display = "none";
+        navOpen = false;
+        document.body.style.overflowY = "auto";
+        navBtn.style.scale = "100%";
+    } else {
+        navOut.style.display = "block";
+        navOpen = true;
+        window.scrollTo(0,0);
+        document.body.style.overflowY = "hidden";
+        navBtn.style.scale = "115%";
+    }
+}
