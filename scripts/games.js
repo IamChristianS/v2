79243afd9b152df3133325b6cbb1e12a01de7dce
unbl0ck <95893289+unbl0ck@users.noreply.github.com>
2024-01-20 18:15:36 -0600

    buncha work

--- scripts/games.js
+++ scripts/games.js
@@ -1,21 +1,29 @@
-// ty to a random github user for this one
+// ty to a random github user for most of the code on this one
 // in case yall cant tell, my ass is NOT fluent in JS
-const Frame = document.querySelector(".games");
-
 async function addGames() {
     try {
-        const cdn = await (await fetch("CDN.json")).json();
+        const cdn = "https://unbl0ck.github.io/assets/";
         const games = await (await fetch(cdn + "list_games.json")).json();
-        games.sort((a, b) => a.game.localeCompare(b.game));
+        games.sort((a, b) => a.gameName.localeCompare(b.gameName));
 
         for (const game of games) {
             const tab = document.createElement("div");
             tab.className = "games-tab";
-            tab.onclick = "window.location.href='${cdn}${game.gameIndex}','_blank'"; // Only for Testing
-            tab.innerHTML = `<img src="${cdn}img/${game.gameImg}.png"><h2>${game.gameName}</h2>`;
-            document.querySelector(".games").appendChild(tab);
+            tab.innerHTML = `
+                <img src="${cdn}img/games/${game.gameName.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()}.png" loading="lazy" onerror="this.src='../img/games/placeholder.png'">
+                <h2>${game.gameName}</h2>
+            `;
+
+            tab.addEventListener('click', () => {
+                localStorage.setItem('srcGame', `${cdn}${game.gameIndex}`);
+                window.location.href = 'player.html';
+            });
+
+            document.getElementById("games").appendChild(tab);
         }
     } catch (error) {
         console.error(error);
     }
 }
+
+addGames();
